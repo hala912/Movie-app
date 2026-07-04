13a123519c9305762e567a88cf56fc61693c7807
hala912 <hala19717131@gmail.com>
2026-07-04 17:11:04 +0300

    Fixing All movie button issue

--- src/componants/categoryoption/categoryoption.tsx
+++ src/componants/categoryoption/categoryoption.tsx
@@ -1,37 +1,46 @@
 import { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import fetchMovieGenre from "../../store/movieslice/actions/getMovieGenre";
-import "./categoryoption.css"
+import "./categoryoption.css";
 
 interface CategoryOptionProps {
-  onClick: (id: number) => void;
-}   
-
-const CategoryOption  = ({ onClick }: CategoryOptionProps) => {
-
-    const genres = useAppSelector((state)=>(state.movies.MovieGenre))
-   const dispatch = useAppDispatch();
-    const [activetab, setActiveTab] = useState<string | null>(null);
-   useEffect(()=>{
-    dispatch(fetchMovieGenre())
-   },[])
-   
-return(
-    <div className="category-options">
-    {genres?.map((MovieGenre:any) => (
-      <button key={MovieGenre.id}
-        className={activetab === MovieGenre.id ? "active" : ""}
-        onClick={() => {
-          setActiveTab(MovieGenre.id);
-          onClick(MovieGenre.id);
-        }}
-      >
-        {MovieGenre.name}
-      </button>
-    ))}
-  </div>
-)
-
+  onClick: (id: number | null) => void;
 }
 
-export default CategoryOption ;
+const CategoryOption = ({ onClick }: CategoryOptionProps) => {
+  const genres = useAppSelector((state) => state.movies.MovieGenre);
+  const dispatch = useAppDispatch();
+  const [activetab, setActiveTab] = useState<string | null>(null);
+  useEffect(() => {
+    dispatch(fetchMovieGenre());
+    
+  }, []);
+
+  return (
+    <div className="category-options">
+      <button
+        className={activetab === null ? "active" : ""}
+        onClick={() => {
+          setActiveTab(null);
+          onClick(null);
+        }}
+      >
+        All Movies
+      </button>
+      {genres?.map((MovieGenre: any) => (
+        <button
+          key={MovieGenre.id}
+          className={activetab === MovieGenre.id ? "active" : ""}
+          onClick={() => {
+            setActiveTab(MovieGenre.id);
+            onClick(MovieGenre.id);
+          }}
+        >
+          {MovieGenre.name}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default CategoryOption;
